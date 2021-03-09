b945dc30f1c3b695e29908901be4092b948613de
eincipientinfo <eincipientinfo@gmail.com>
2021-03-09 14:22:17 +0530

    Nuxt Js

---
+++ common/button/ButtonDanger.vue
@@ -0,0 +1,40 @@
+<template>
+  <nuxt-link :to="prop.path">
+    <b-button class="btn-danger" :class="prop.addBtnClass"> {{ prop.text }} </b-button>
+  </nuxt-link>
+</template>
+<script>
+export default {
+  props: ["prop"]
+};
+</script>
+<style scoped>
+.btn-danger {
+  border-radius: 60px !important;
+  background: linear-gradient(90deg, #e50612, #730309) !important;
+  color: #fff !important;
+  text-align: center !important;
+  padding: 18px 60px;
+  border: 0;
+  overflow: hidden;
+  position: relative;
+  z-index: 1;
+  font-family: Open Sans, sans-serif !important;
+  font-size: 1.125rem;
+}
+.btn-danger:before {
+  position: absolute;
+  content: "";
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: linear-gradient(90deg, #730309, #e50612) !important;
+  z-index: -1;
+  transition: opacity 0.5s linear;
+  opacity: 0;
+}
+.btn-danger:hover:before {
+  opacity: 1;
+}
+</style>
---
+++ home/credentials/Carousel.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <VueSlickCarousel v-bind="carouselSettings">
+      <div v-for="(credential, index) in prop" :key="index">
+        <Credential :prop="credential" />
+      </div>
+    </VueSlickCarousel>
+  </div>
+</template>
+<script>
+import Credential from "./Credential";
+export default {
+  props: ["prop"],
+  components: {
+    Credential
+  },
+  data() {
+    return {
+      carouselSettings: {
+        slidesToShow: 3,
+        infinite: false,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
+<!-- This css is for carousel only. if I will add scoped it won't work -->
+<style>
+.slick-arrow {
+  background: #acacac;
+  font-weight: 900 !important;
+  border-radius: 50%;
+}
+.slick-prev:hover,
+.slick-prev:focus,
+.slick-next:hover,
+.slick-next:focus {
+  background: #acacac;
+}
+.slick-next,
+.slick-prev {
+  padding-top: 1px !important;
+}
+</style>
